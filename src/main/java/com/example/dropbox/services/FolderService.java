6afb4a99e4a474a65d975fd9be5bfd9d07a14b04
EmilNilsson00 <emilnilsson00@icloud.com>
2023-12-09 16:10:34 +0100

    Added createFolder method

--- src/main/java/com/example/dropbox/services/FolderService.java
+++ src/main/java/com/example/dropbox/services/FolderService.java
@@ -1,5 +1,6 @@
 package com.example.dropbox.services;
 
+import com.example.dropbox.models.Folder;
 import com.example.dropbox.models.User;
 import com.example.dropbox.repositories.FolderRepository;
 import com.example.dropbox.security.CheckAuth;
@@ -11,6 +12,23 @@ public class FolderService {
 
     @Autowired
     private FolderRepository folderRepository;
+
+    public
     User user = CheckAuth.checkAuth();
 
+    public Folder createFolder(Folder folderRequest) throws IllegalArgumentException {
+        User user = CheckAuth.checkAuth();
+
+        if (folderRequest == null || folderRequest.getName() == null || folderRequest.getName().isEmpty()) {
+            throw new IllegalArgumentException("Invalid folder name.");
+        }
+
+        Folder folder = Folder.builder()
+                .name(folderRequest.getName())
+                .user(user)
+                .build();
+        folderRepository.save(folder);
+
+        return folder;
+    }
 }
